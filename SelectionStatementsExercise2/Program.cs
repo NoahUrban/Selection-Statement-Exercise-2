404870ada50a14b04b5d7f04dc5330ae970fda79
“Noah <urbannoah@yahoo.com>
2024-06-05 11:08:44 -0400

    selection statement exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your favorite subject out of the choices below...\n" +
+                "Math\n" +
+                "Science\n" +
+                "History\n" +
+                "English\n" +
+                "Physical Education\n");
+            string favSub = Console.ReadLine();
+
+            switch (favSub)
+            {
+                case "Math":
+                    Console.WriteLine();
+                    Console.WriteLine($"{favSub} is useful in fields such as engineering and phsyics!");
+                    break;
+                case "Science":
+                    Console.WriteLine();
+                    Console.WriteLine($"{favSub} is useful in fields such as biology and health sciences!");
+                    break;
+                case "History":
+                    Console.WriteLine();
+                    Console.WriteLine($"{favSub} is useful in a field such as archeology");
+                    break;
+                case "English":
+                    Console.WriteLine();
+                    Console.WriteLine($"{favSub} was my least favorite subject!");
+                    break;
+                case "Physical Education":
+                    Console.WriteLine();
+                    Console.WriteLine($"{favSub} is a great way to learn about the body and stay in shape!");
+                    break;
+                default:
+                    Console.WriteLine();
+                    Console.WriteLine("You did not enter one of the choices listed");
+                    break;
+            }
+
         }
     }
 }
